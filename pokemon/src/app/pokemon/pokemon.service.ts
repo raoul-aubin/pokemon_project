3539cc782515de609663e15945cceb0de6eac49c
Raoul Tchangou <aubinkamtcheu70@gmail.com>
2026-03-09 00:19:34 +0100

    UPDATE

--- pokemon/src/app/pokemon/pokemon.service.ts
+++ pokemon/src/app/pokemon/pokemon.service.ts
@@ -9,24 +9,25 @@ import { Pokemon } from './pokemon';
 })
 export class PokemonService {
 
+  private api = 'http://localhost:8080/pokemon_backend/api';
   constructor(private http: HttpClient) {}
 
   getPokemonList(): Observable<Pokemon[]> {
-    return this.http.get<Pokemon[]>('http://localhost:8080/pokemon_backend/api/pokemons').pipe(
+    return this.http.get<Pokemon[]>(`${this.api}/pokemons`).pipe(
       tap(response => this.log(response)),
       catchError(error => this.handleError(error, []))
     );
   }
 
   getPokemonById(pokemonId: number): Observable<Pokemon | undefined> {
-    return this.http.get<Pokemon>(`http://localhost:8080/pokemon_backend/api/pokemons/${pokemonId}`).pipe(
+    return this.http.get<Pokemon>(`${this.api}/pokemons/${pokemonId}`).pipe(
       tap(response => this.log(response)),
       catchError(error => this.handleError(error, undefined))
     );
   }
 
   getPokemonByOwner(): Observable<Pokemon[]> {
-    return this.http.get<Pokemon[]>('http://localhost:8080/pokemon_backend/api/me/pokemons').pipe(
+    return this.http.get<Pokemon[]>(`${this.api}/me/pokemons`).pipe(
       tap(response => this.log(response)),
       catchError(error => this.handleError(error, []))
     );
@@ -37,7 +38,7 @@ export class PokemonService {
       return of([]);
     }
 
-    return this.http.get<Pokemon[]>(`http://localhost:8080/pokemon_backend/api/pokemons/search?name=${term}`).pipe(
+    return this.http.get<Pokemon[]>(`${this.api}/pokemons/search?name=${term}`).pipe(
       tap(response => this.log(response)),
       catchError(error => this.handleError(error, []))
     );
@@ -48,7 +49,7 @@ export class PokemonService {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     };
 
-    return this.http.post<Pokemon>('http://localhost:8080/pokemon_backend/api/pokemons', pokemon, httpOptions).pipe(
+    return this.http.post<Pokemon>(`${this.api}/pokemons`, pokemon, httpOptions).pipe(
       tap(response => this.log(response)),
       catchError(error => this.handleError(error, pokemon))
     );
@@ -59,14 +60,14 @@ export class PokemonService {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     };
 
-    return this.http.put<null>(`http://localhost:8080/pokemon_backend/api/pokemons/${pokemon.id}`, pokemon, httpOptions).pipe(
+    return this.http.put<null>(`${this.api}/pokemons/${pokemon.id}`, pokemon, httpOptions).pipe(
       tap(response => this.log(response)),
       catchError(error => this.handleError(error, null))
     );
   }
 
   deletePokemonById(pokemonId: number): Observable<null> {
-    return this.http.delete<null>(`http://localhost:8080/pokemon_backend/api/pokemons/${pokemonId}`).pipe(
+    return this.http.delete<null>(`${this.api}/pokemons/${pokemonId}`).pipe(
       tap(response => this.log(response)),
       catchError(error => this.handleError(error, null))
     );
--- pokemon/src/app/user/user.service.ts
+++ pokemon/src/app/user/user.service.ts
@@ -12,31 +12,31 @@ export interface ChangePasswordRequest {
   providedIn: 'root',
 })
 export class UserService {
-  private api = 'http://localhost:8080/pokemon_backend/api/me';
+  private api = 'http://localhost:8080/pokemon_backend/api';
 
   constructor(private http: HttpClient) {}
 
   getUser(): Observable<User | null> {
-    return this.http.get<User>(`${this.api}`).pipe(
+    return this.http.get<User>(`${this.api}/me`).pipe(
       tap(response => this.log(response)),
       catchError(error => this.handleError(error, null))
     );
   }
 
   updateUser(user: User): Observable<void> {
-    return this.http.put<void>(`${this.api}`, user).pipe(
+    return this.http.put<void>(`${this.api}/me`, user).pipe(
       catchError(error => this.handleError(error, undefined))
     );
   }
 
   deleteUser(): Observable<void> {
-    return this.http.delete<void>(`${this.api}`).pipe(
+    return this.http.delete<void>(`${this.api}/me`).pipe(
       catchError(error => this.handleError(error, undefined))
     );
   }
 
   changePassword(req: ChangePasswordRequest): Observable<void> {
-    return this.http.put<void>(`${this.api}/password`, req).pipe(
+    return this.http.put<void>(`${this.api}/me/password`, req).pipe(
       catchError(error => this.handleError(error, undefined))
     );
   }
